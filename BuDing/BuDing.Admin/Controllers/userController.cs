71091963fee93276a28e089b96668a53ffb10722
godric <miaochenGod@outlook.com>
2018-11-28 23:42:26 +0800

    增加user/login接口

---
+++ BuDing/BuDing.Admin/Controllers/userController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuDing.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+		[HttpGet("login")]
+		[HttpPost("login")]
+		public ActionResult Login(string username,string password)
+		{
+			return new JsonResult(new { data = new { token = "1111111" } });
+		}
+    }
+}
