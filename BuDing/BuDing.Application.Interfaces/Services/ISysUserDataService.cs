94cec28fc9b3bb7783c242bd50fa688db40daf24
godric <miaochenGod@outlook.com>
2018-12-09 19:59:25 +0800

    增加SysyUserService

--- BuDing/BuDing.Application.Interfaces/Services/ISysUserDataService.cs
+++ BuDing/BuDing.Application.Interfaces/Services/ISysUserDataService.cs
@@ -3,23 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using BuDing.Domain.Entities;
 using BuDing.Infrastructure.DataService;
+using BuDing.Infrastructure.PageList;
 
 namespace BuDing.Application.Interfaces.Services
 {
     public interface ISysUserDataService:IService<SysUserEntity>
     {
-        Task<SysUserEntity> GetSysUserById(int id);
+        IPagedList<SysUserEntity> GetPagedList(int pageIndex, int pageSize);
 
-
-        Task<SysUserEntity> Insert(SysUserEntity entity);
-
-        Task<SysUserEntity> Update(SysUserEntity entity);
-
-        Task Delete(SysUserEntity entity);
-
-        Task<IPagedList<SysUserEntity>> GetPagedList(int pageIndex, int pageSize);
-
-        Task<IPagedList<SysUserEntity>> GetPagedListByUserName(string userName, int pageIndex, int pageSize);
+        IPagedList<SysUserEntity> GetPagedListByUserName(string userName, int pageIndex, int pageSize);
 
     }
 }
