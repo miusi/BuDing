2a1c84840ba0deae6fb9011c1f786aea68fccb6d
godric <miaochenGod@outlook.com>
2018-12-18 23:35:34 +0800

    SysUser修改Get和Post方法

--- BuDing/BuDing.Admin/Controllers/SysUserController.cs
+++ BuDing/BuDing.Admin/Controllers/SysUserController.cs
@@ -6,6 +6,7 @@ using BuDing.Application.Interfaces.PageList;
 using BuDing.Application.Interfaces.Services;
 using BuDing.Domain.Entities;
 using BuDing.Infrastructure.Utilities;
+using BuDing.Infrastructure.ValidationLogic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,7 +29,7 @@ namespace BuDing.Admin.Controllers
 		/// <param name="sysUserService">系统用户服务对象</param> 
 		public SysUserController(ISysUserService sysUserService)
 		{
-			this.sysUserService = sysUserService; 
+			this.sysUserService = sysUserService;
 
 		}
 
@@ -38,36 +39,53 @@ namespace BuDing.Admin.Controllers
 		/// <returns></returns>
 		// GET: api/SysUser
 		[HttpGet]
-		public IActionResult  Get()
+		public IActionResult Get()
 		{
 			JsonResult<IPagedList<SysUserEntity>> result = new JsonResult<IPagedList<SysUserEntity>>();
 			result.Status = true;
 			result.StatusCode = "200";
-			result.Data = sysUserService.Find().ToPagedList<SysUserEntity>(0,20); 
+			result.Data = sysUserService.Find().ToPagedList<SysUserEntity>(0, 20);
 			return Ok(result);
 		}
 
-	 
 
+		/// <summary>
+		/// 通过Id获取系统用户实体
+		/// </summary>
+		/// <param name="id">id</param>
+		/// <returns></returns>
 		// GET: api/SysUser/5
 		[HttpGet("{id}", Name = "Get")]
-		public SysUserEntity Get(int id)
+		public IActionResult Get(int id)
 		{
-			return sysUserService.Get(id);
+			JsonResult<SysUserEntity> result = new JsonResult<SysUserEntity>();
+			result.Status = true;
+			result.StatusCode = "200";
+			result.Data = sysUserService.Get(id);
+			return Ok(result);
 		}
 
 		/// <summary>
 		/// 新增
 		/// </summary>
-		/// <param name="entity">系统</param>
+		/// <param name="entity">系统用户</param>
 		// POST: api/SysUser
 		[HttpPost]
-		public void Post([FromBody] SysUserEntity entity)
+		public IActionResult Post([FromBody] SysUserEntity entity)
 		{
+			JsonResult<ValidationResult> jsonResult = new JsonResult<ValidationResult>();
 			if (entity != null)
-			{  
-				sysUserService.Add(entity);
+			{
+				var validationResult = sysUserService.Add(entity);
+				jsonResult.Data = validationResult;
 			}
+			else
+			{
+				ValidationResult validationResult = new ValidationResult();
+				validationResult.Add("新增数据不能为空");
+				jsonResult.Data = validationResult;
+			}
+			return Ok(jsonResult);
 		}
 
 		// PUT: api/SysUser/5
@@ -91,7 +109,7 @@ namespace BuDing.Admin.Controllers
 		/// <returns></returns>
 		[HttpPost]
 		[Route("api/SysUser/Login")]
-		public IActionResult Login(string username,string password)
+		public IActionResult Login(string username, string password)
 		{
 			return Ok(sysUserService.Login(username, password));
 		}
