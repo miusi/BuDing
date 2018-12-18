da16a15c30be05c92e4d25e1ef752ae4597dd3f1
godric <miaochenGod@outlook.com>
2018-12-18 23:01:30 +0800

    增加返回值BaseResult

--- BuDing/BuDing.Infrastructure/Utilities/BaseResult.cs
+++ BuDing/BuDing.Infrastructure/Utilities/BaseResult.cs
@@ -23,15 +23,63 @@ namespace BuDing.Infrastructure.Utilities
 		public string StatusCode = "UnknownError";
 
 		/// <summary>
-		/// 返回消息内容
+		/// 验证结果
 		/// </summary>
-		public string StatusMessage = "未知错误";
-				
 		public ValidationResult ValidationResult { get; set; }
+		
+		/// <summary>
+		/// cotr
+		/// </summary>
+		public BaseResult()
+		{
+			this.Status = false;
+			this.ValidationResult = new ValidationResult();
+		}
 
 		/// <summary>
-		/// 查询分页数据时返回记录条数用
+		/// 输出成功消息
 		/// </summary>
-		public int RecordCount = 0;
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public static BaseResult Success()
+		{
+			return new BaseResult()
+			{
+				Status = true
+			};
+		}
+
+		/// <summary>
+		/// 输出失败消息
+		/// </summary>
+		/// <returns></returns>
+		public static BaseResult Fail()
+		{
+			return new BaseResult()
+			{
+				Status = false
+			};
+		}
+
+		/// <summary>
+		/// 输入错误信息
+		/// </summary>
+		/// <returns></returns>
+		public static BaseResult Error()
+		{
+			return new BaseResult()
+			{
+				Status = false
+			};
+		}
+	}
+
+	/// <summary>
+	/// json
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class JsonResult<T> : BaseResult
+	{
+		public T Data { get; set; }
 	}
 }
